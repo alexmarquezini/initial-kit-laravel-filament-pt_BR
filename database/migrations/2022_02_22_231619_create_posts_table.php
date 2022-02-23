00b953a9e55bf16008e38c25f90449902f720dee
Alex Marquezini <alexsmarquez@hotmail.com>
2022-02-23 00:16:48 -0300

    Criação dos Models e Migrations

---
+++ database/migrations/2022_02_22_231619_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('title');
+            $table->text('summary')->nullable();
+            $table->text('body')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->string('featured_image')->nullable();
+            $table->string('featured_image_caption')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->json('meta');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2022_02_22_231817_create_user_metas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_metas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('username')->nullable();
+            $table->text('summary')->nullable();
+            $table->string('avatar')->nullable();
+            $table->boolean('dark_mode')->nullable();
+            $table->boolean('digest')->nullable();
+            $table->string('locale')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_metas');
+    }
+};
